fbae9efd931c867833c582a91c87fbd68744d4e4
Iván <inbobarini@gmail.com>
2024-04-23 08:48:48 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<input type="text" bind:value={inputValue}>
+<button on:click={createForm}>Click me</button>
+
+<script>
+    let entitiesKeys = {requirements:[
+        "name", "status", "reqSource", "value", "toleranceUp", "toleranceDown"
+    ]}
+        
+    let inputValue = ""
+    function createForm(){
+        
+    }
+
+</script>
+<style>
+
+</style>
